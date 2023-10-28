57e3526f61bc586999ea4c88aa2ab4a4faf6c755
Yannik Inniger <yannik.inniger@ti8m.ch>
2023-10-28 15:01:36 +0200

    Implement datepicker 📆

---
+++ src/lib/Datepicker.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+   export let label: string = 'Select a date please';
+   export let date: Date;
+</script>
+
+<div>
+   <label for="date">{label}</label>
+   <input type="date" name="date" bind:value={date} />
+</div>
+
+<style>
+   div {
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+   }
+
+   label {
+      margin-bottom: 0.25rem;
+   }
+</style>
