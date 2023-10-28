e048efd046f58fbd34f3ce6d0bc2a6b792efa353
Arii2727 <149237806+Arii2727@users.noreply.github.com>
2023-10-28 18:18:58 +0200

    The Lost&Found Flow is flowing ;)

---
+++ src/routes/lost-and-found/SearchInFound.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+   // logic
+</script>
+
+<div class="boxes">
+   <div>Lost</div>
+   <div>Found</div>
+</div>
+
+<style>
+   .boxes {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+   }
+</style>
+
