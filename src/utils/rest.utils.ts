58bf983c534785e86bcff1cc7f12a477487af204
Yannik Inniger <yannik.inniger@ti8m.ch>
2023-10-29 01:28:34 +0200

    Load user from backend, thanks to the 🦫

--- src/utils/rest.utils.ts
+++ src/utils/rest.utils.ts
@@ -1,15 +1,15 @@
 import { BASE_URL } from '../consts';
 
-export const get = (endpoint: string) => {
-   fetch(`${BASE_URL}${endpoint}`, {
+export const get = async <T>(endpoint: string) => {
+   return fetch(`${BASE_URL}${endpoint}`, {
       method: 'GET',
-   }).then((response: Response) => response.json());
+   }).then((response: Response) => response.json() as T);
 };
 
-export const post = <T>(endpoint: string, body: T): Promise<T> => {
+export const post = async <T>(endpoint: string, body: T): Promise<T> => {
    const json = JSON.stringify(body);
    return fetch(`${BASE_URL}${endpoint}`, {
       method: 'POST',
       body: json,
-   }).then((response: Response) => response.json());
+   }).then((response: Response) => response.json() as T);
 };
